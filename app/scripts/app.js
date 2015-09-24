1b6b96f9377d36b2606ea1a935f92590f9bfc549
John Foushee <johnfoushee@gmail.com>
2015-09-24 15:44:31 -0400

    Adding mobile support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -122,7 +122,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // Scroll page to top and expand header
   app.scrollPageToTop = function() {
-    document.getElementById('mainContainer').scrollTop = 0;
+    //document.getElementById('mainContainer').scrollTop = 0;
   };
 
 
